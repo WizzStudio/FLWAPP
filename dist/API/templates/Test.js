5810e069ba2478cf24b2f1ee1d90063552d73421
ACERY1 <y841034081@gmail.com>
2018-02-10 19:37:02 +0800

    clear dist file

--- dist/API/templates/Test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-	value: true
-});
-exports.default = {
-	'list|1-10': [{ 'id|10': 1 }]
-};
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIlRlc3QuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7a0JBQWU7QUFDZCxjQUFhLENBQ1osRUFBQyxTQUFTLENBQVYsRUFEWTtBQURDLEMiLCJmaWxlIjoiVGVzdC5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IHtcblx0J2xpc3R8MS0xMCc6IFtcblx0XHR7J2lkfDEwJzogMX1cblx0XVxufVxuIl19
