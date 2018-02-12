48efc39bd555b5aac730e3b1efdf62ef72fb22de
ACERY1 <y841034081@gmail.com>
2018-02-12 16:59:47 +0800

    add sass module :sparkles:

---
+++ dist/common/scripts/example.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+function add(a, b) {
+	return a + b;
+}
+
+exports.add = add;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImV4YW1wbGUuanMiXSwibmFtZXMiOlsiYWRkIiwiYSIsImIiXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsU0FBU0EsR0FBVCxDQUFjQyxDQUFkLEVBQWlCQyxDQUFqQixFQUFvQjtBQUNuQixRQUFPRCxJQUFJQyxDQUFYO0FBQ0E7O1FBRVFGLEcsR0FBQUEsRyIsImZpbGUiOiJleGFtcGxlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiZnVuY3Rpb24gYWRkIChhLCBiKSB7XG5cdHJldHVybiBhICsgYlxufVxuXG5leHBvcnQgeyBhZGQgfVxuIl19
