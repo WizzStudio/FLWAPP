9dcb9fa9dc56e92babcff823b515292f0bc436e9
ACERY1 <y841034081@gmail.com>
2018-02-13 14:36:41 +0800

    init component :construction: & delete dist file (except dist/static) :recycle:

--- dist/common/scripts/utils.js
+++
@@ -1,3 +0,0 @@
-/* 工具函数 */
-"use strict";
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInV0aWxzLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBIiwiZmlsZSI6InV0aWxzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiLyog5bel5YW35Ye95pWwICovXG4iXX0=
--- dist/common/scripts/wxUtil.js
+++
@@ -1,3 +0,0 @@
-/* 微信接口 */
-"use strict";
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInd4VXRpbC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSIsImZpbGUiOiJ3eFV0aWwuanMiLCJzb3VyY2VzQ29udGVudCI6WyIvKiDlvq7kv6HmjqXlj6MgKi9cbiJdfQ==
